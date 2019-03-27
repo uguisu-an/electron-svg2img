20cae7e9fc4d82169957acc705accf6b4c11808f
uguisu-an <arcana278@gmail.com>
2019-03-27 21:11:45 +0900

    キャプチャして入れた

--- app/main.js
+++ app/main.js
@@ -1,11 +1,46 @@
 const { BrowserWindow, app } = require("electron");
+const DataURI = require("datauri");
+
+function captureImage(url) {
+  return new Promise(resolve => {
+    const w = new BrowserWindow({
+      width: 640,
+      height: 180,
+      opacity: 0,
+      useContentSize: true
+    });
+    w.webContents.on("did-finish-load", () => {
+      // Wait for loading svg
+      setTimeout(() => {
+        w.webContents.capturePage(image => {
+          resolve(image);
+        });
+      }, 100);
+    });
+    w.loadURL(url);
+  });
+}
 
 let win;
 
 app.on("ready", () => {
   win = new BrowserWindow({
     width: 800,
-    height: 640
+    height: 640,
+    webPreferences: {
+      nodeIntegration: true
+    }
   });
+  win.webContents.on("new-window", (e, url) => {
+    if (/^blob:/.test(url)) {
+      e.preventDefault();
+      captureImage(url).then(image => {
+        const datauri = new DataURI();
+        datauri.format(".png", image.toPNG());
+        win.send("put-image", datauri.content);
+      });
+    }
+  });
+  win.webContents.openDevTools();
   win.loadFile(`${__dirname}/index.html`);
 });
--- app/renderer.js
+++ app/renderer.js
@@ -1,12 +1,27 @@
+const { ipcRenderer } = require("electron");
+
+function createSVGURL() {
+  const svg = document.getElementById("svg");
+  const xml = new XMLSerializer().serializeToString(svg);
+  const blob = new Blob([xml], { type: "image/svg+xml" });
+  return URL.createObjectURL(blob);
+}
+
 function createImage() {
-  const e1 = document.getElementById("svg");
-  const svg = new XMLSerializer().serializeToString(e1);
-  const blob = new Blob([svg], { type: "image/svg+xml" });
-  const url = URL.createObjectURL(blob);
-  const e2 = document.getElementById("img");
-  e2.src = url;
+  const img = document.getElementById("img1");
+  img.src = createSVGURL();
+}
+
+function requestCaptureImage() {
+  open(createSVGURL());
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   createImage();
+  requestCaptureImage();
+});
+
+ipcRenderer.on("put-image", (_event, src) => {
+  const img = document.getElementById("img2");
+  img.src = src;
 });
